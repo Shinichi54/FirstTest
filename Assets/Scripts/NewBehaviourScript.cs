50a77e6d6799fe690ef1e9e5f4fc3922e14cef23
勾勾 <gougou@16fan.com>
2017-10-27 18:12:08 +0800

    第一次提交

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    private AndroidJavaObject _input = null;
+    private Transform canvasTras;
+    private Text text;
+    private Button btn;
+    private InputField inputField;
+
+    void Start()
+    {
+		//AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+		//jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+
+		_input = new AndroidJavaObject("com.gg.unity_exchange.AndroidKeyboard");
+
+        canvasTras = GameObject.Find("Canvas").transform;
+        text = canvasTras.Find("Text").GetComponent<Text>();
+        btn = canvasTras.Find("Button").GetComponent<Button>();
+        inputField = canvasTras.Find("InputField").GetComponent<InputField>();
+
+        _input.Call("Open", text.text, false);
+    }
+
+    void OnCustomInputAction(string data)
+    {
+        text.text = data.ToString();
+    }
+}
